5c4496d447d69e87a4d0b8ff403ffbdd39147d2d
Kingloco1990 <ransfordnawelbeck@gmail.com>
2024-10-25 15:10:21 +0000

    My website portfolio

---
+++ script.js
@@ -0,0 +1,7 @@
+function showSidebar() {
+    const sideIcon = document.querySelector(".sidebar-icon");
+    const sideLinks = document.querySelector(".sidebar-links");
+
+    sideIcon.classList.toggle("open");
+    sideLinks.classList.toggle("open");
+}
